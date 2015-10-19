aee4848f3d6cee03e5071452071ed87f239f2fcf
Samori Gorse <samorigorse@gmail.com>
2015-10-19 10:27:30 +0200

    Added init command

--- bin/sardine.jsx
+++ bin/sardine.jsx
@@ -1,10 +1,16 @@
 import program from 'commander';
 
+import init from './commands/init';
 import create from './commands/create';
 
 program
   .version(require('../package.json').version)
   .option('-V, --verbose', 'Display verbose information', false);
+
+program
+  .command('init')
+  .description('Initialize a new Sardine project')
+  .action(init);
 
 program
   .command('create <suffix>')
