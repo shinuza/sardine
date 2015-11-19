f67578919c7b068d2c65d77aba80addadde43f8b
Samori Gorse <samorigorse@gmail.com>
2015-11-19 16:11:04 +0100

    Linted every file in lib/
    Also, fixed two errors in lib/db/model.jsx

--- lib/db/model.jsx
+++ lib/db/model.jsx
@@ -50,7 +50,7 @@ export default class Model {
   }
 
   insert(values) {
-    const { name, applied, migration_time, checksum } = values; // eslint-disable camelCase
+    const { name, applied, migration_time, checksum } = values; // eslint-disable-line camelcase
 
     return this.connect()
       .then(() => {
