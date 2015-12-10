d7361b948d1adfd540041ece98c04178fdf4d32d
Samori Gorse <samorigorse@gmail.com>
2015-12-10 16:03:02 +0100

    Handle MigrationNotFound error properly

--- bin/commands/step.jsx
+++ bin/commands/step.jsx
@@ -1,12 +1,16 @@
 import Sardine from '../../lib';
 import { events } from '../../lib/events';
+import { showError } from '../util';
+import { MigrationNotFound } from '../../lib/errors';
 
 function step(config, migrationName, suffixes) {
   const sardine = new Sardine(config);
 
   sardine.on(events.STEP_FILE_CREATED, sardine.onStepFileCreated);
 
-  return sardine.step(migrationName, suffixes);
+  return sardine
+    .step(migrationName, suffixes)
+    .catch(MigrationNotFound, (e) => showError(e.message));
 }
 
 export default step;
